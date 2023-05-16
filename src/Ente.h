4897f0e03f364a9f39be422a6b7cd5fb0b986db6
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-05-16 20:50:15 -0300

    adiciona inicializaSprite

--- src/Ente.h
+++ src/Ente.h
@@ -1,15 +1,45 @@
 #pragma once
+#include "SFML/Graphics/Sprite.hpp"
+#include "SFML/Graphics/Texture.hpp"
+#include <stdexcept>
 
 namespace Jogo {
 class Ente {
 public:
-  Ente();
+  Ente() : pFig(nullptr) {}
   ~Ente();
 
   virtual void executar() = 0;
-  void imprimir(){}; // o que eh suposto isso fazer?
+  void desenhar(){};
 
 protected:
   int id; // pode trocar para um enum?
+  sf::Sprite *pFig;
+  sf::Texture textura;
+
+  void inicializaSprite(const char *caminhoTextura) {
+    if (pFig != nullptr)
+      return;
+
+    carregaTextura(caminhoTextura);
+    pFig = new sf::Sprite(textura);
+
+    if (!pFig)
+      throw std::runtime_error("Nao foi possivel criar sprite");
+  }
+
+  void inicializaSprite(const char *caminhoTextura, sf::IntRect limite) {
+    if (pFig != nullptr)
+      return;
+
+    carregaTextura(caminhoTextura);
+    pFig = new sf::Sprite(textura, limite);
+
+    if (!pFig)
+      throw std::runtime_error("Nao foi possivel criar sprite");
+  }
+
+private:
+  void carregaTextura(const char *caminho) { textura.loadFromFile(caminho); }
 };
 } // namespace Jogo
