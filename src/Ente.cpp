506ee54b5f964b9cf4639d5df826eab85f376055
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-05-16 20:52:44 -0300

    move implementações do Ente para cpp

--- src/Ente.cpp
+++ src/Ente.cpp
@@ -1,6 +1,28 @@
 #include "Ente.h"
 
 namespace Jogo {
-Ente::Ente() = default;
+Ente::Ente() : pFig(nullptr) {}
 Ente::~Ente() = default;
+
+void Ente::inicializaSprite(const char *caminhoTextura) {
+  if (pFig != nullptr)
+    return;
+
+  carregaTextura(caminhoTextura);
+  pFig = new sf::Sprite(textura);
+
+  if (!pFig)
+    throw std::runtime_error("Nao foi possivel criar sprite");
+}
+
+void Ente::inicializaSprite(const char *caminhoTextura, sf::IntRect limite) {
+  if (pFig != nullptr)
+    return;
+
+  carregaTextura(caminhoTextura);
+  pFig = new sf::Sprite(textura, limite);
+
+  if (!pFig)
+    throw std::runtime_error("Nao foi possivel criar sprite");
+}
 } // namespace Jogo
--- src/Ente.h
+++ src/Ente.h
@@ -6,7 +6,7 @@
 namespace Jogo {
 class Ente {
 public:
-  Ente() : pFig(nullptr) {}
+  Ente();
   ~Ente();
 
   virtual void executar() = 0;
@@ -17,27 +17,8 @@ protected:
   sf::Sprite *pFig;
   sf::Texture textura;
 
-  void inicializaSprite(const char *caminhoTextura) {
-    if (pFig != nullptr)
-      return;
-
-    carregaTextura(caminhoTextura);
-    pFig = new sf::Sprite(textura);
-
-    if (!pFig)
-      throw std::runtime_error("Nao foi possivel criar sprite");
-  }
-
-  void inicializaSprite(const char *caminhoTextura, sf::IntRect limite) {
-    if (pFig != nullptr)
-      return;
-
-    carregaTextura(caminhoTextura);
-    pFig = new sf::Sprite(textura, limite);
-
-    if (!pFig)
-      throw std::runtime_error("Nao foi possivel criar sprite");
-  }
+  void inicializaSprite(const char *caminhoTextura);
+  void inicializaSprite(const char *caminhoTextura, sf::IntRect limite);
 
 private:
   void carregaTextura(const char *caminho) { textura.loadFromFile(caminho); }
