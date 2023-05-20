cbce76fea2ae97ef6dfecc6290930d30f85742e2
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-05-20 12:48:10 -0300

    adapta main para rodar com novas classes

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,31 @@
+#include "Gerenciador_Grafico.h"
 #include "entidades/Jogador.h"
 #include <SFML/Graphics.hpp>
 
+#define CAMINHO_ASSETS "./assets"
+#define CAMINHO_FONTES CAMINHO_ASSETS "/fonts"
+#define CAMINHO_IMAGENS CAMINHO_ASSETS "/images"
+
 int main() {
-  sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
-  window.setFramerateLimit(60);
+  auto pGerenciadorGrafico =
+      Jogo::Gerenciadores::Gerenciador_Grafico::getInstancia();
 
-  Jogo::Entidades::Personagens::Jogador teste;
+  Jogo::Entidades::Personagens::Jogador teste(CAMINHO_IMAGENS
+                                              "/player-idle.png");
 
-  while (window.isOpen()) {
+  while (pGerenciadorGrafico->verificaJanelaAberta()) {
     sf::Event event;
-    while (window.pollEvent(event)) {
+    while (pGerenciadorGrafico->verificarEvento(event)) {
       if (event.type == sf::Event::Closed)
-        window.close();
+        pGerenciadorGrafico->fecharJanela();
       if (event.type == sf::Event::KeyPressed)
         if (event.key.code == sf::Keyboard::Escape)
-          window.close();
+          pGerenciadorGrafico->fecharJanela();
     }
 
-    window.clear();
-    teste.mover();
-    window.draw(teste.getShape());
-    window.display();
+    pGerenciadorGrafico->limparJanela();
+    pGerenciadorGrafico->desenharEnte(&teste);
+    pGerenciadorGrafico->renderizar();
   }
 
   return 0;
