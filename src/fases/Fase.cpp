fa9cc12960d55288c1179a7f0d632cbaf4be3892
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-05-22 20:31:09 -0300

    move implementação do Fase::carregarMapa para o header

--- src/fases/Fase.cpp
+++ src/fases/Fase.cpp
@@ -1,7 +1,6 @@
 #include "Fase.h"
 #include "../Gerenciador_Grafico.h"
 #include "../entidades/Entidade.h"
-#include "../uteis.h"
 #include <type_traits>
 #include <utility>
 
@@ -15,32 +14,9 @@ void Fase::carregarBackground() {
   inicializaSprite(CAMINHO_IMAGENS "/back.png", {0, 0});
 }
 
-template <class E, typename... Args>
-void Fase::carregarMapa(const char *path, Args... args) {
-  static_assert(std::is_base_of_v<Entidades::Entidade, E>);
-
-  unsigned int indiceLinha = 0;
-  Uteis::lerArquivo(path, [&args..., this, &indiceLinha](auto &linha) {
-    unsigned int indiceEntidade = 0;
-
-    for (auto caractere : linha) {
-      if (caractere == '0')
-        continue;
-
-      E *entidade = new E(std::forward(args...));
-
-      // TODO: adicionar no gerenciador de obstaculos com switch/case usando os
-      // IDs
-
-      listaEntidades.push_back(entidade);
-      entidade->setPosicao(
-          {indiceLinha * TAMANHO_TILE, indiceEntidade * TAMANHO_TILE});
-
-      indiceEntidade++;
-    }
-
-    indiceLinha++;
-  });
+void Fase::executar() {
+  for (auto entidade : listaEntidades) {
+    pGG->desenharEnte(entidade);
+  }
 }
-
 } // namespace Jogo::Fases
--- src/fases/Fase.h
+++ src/fases/Fase.h
@@ -1,5 +1,6 @@
 #include "../Ente.h"
 #include "../entidades/Entidade.h"
+#include "../uteis.h"
 #include <list>
 
 #define CAMINHO_DATA "./data"
@@ -12,14 +13,43 @@ class Fase : public Ente {
 public:
   Fase();
   ~Fase() = default;
-  void executar() override{};
+  void executar() override;
   // void gerenciar_colisoes();
   // void criarInimigosMedios();
   // void criarObstMedios();
 
-protected:
-  template <class E, typename... Args> void carregarMapa(const char *, Args...);
+  template <class E, typename... Args>
+  void carregarMapa(const char *path, Args... args) {
+    static_assert(std::is_base_of_v<Entidades::Entidade, E>);
 
+    unsigned int indiceLinha = 0;
+    Uteis::lerArquivo(path, [&args..., this, &indiceLinha](auto &linha) {
+      unsigned int indiceEntidade = 0;
+
+      for (auto caractere : linha) {
+        if (caractere == '0') {
+          indiceEntidade++;
+          continue;
+        }
+
+        E *entidade = new E(std::forward<Args>(args)...);
+
+        // TODO: adicionar no gerenciador de obstaculos com switch/case usando
+        // os IDs
+
+        listaEntidades.push_back(entidade);
+        entidade->setEscalaFigura(2, 2);
+        entidade->setPosicao(
+            {indiceEntidade * TAMANHO_TILE, indiceLinha * TAMANHO_TILE});
+
+        indiceEntidade++;
+      }
+
+      indiceLinha++;
+    });
+  }
+
+protected:
   std::list<Entidades::Entidade *> listaEntidades;
 
 private:
