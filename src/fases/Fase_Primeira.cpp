f7654c3e5edf8620bc0cac621679b16cecbde7fb
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-06-05 17:07:50 -0300

    altera o inimigo fácil para um sapo

--- src/fases/Fase_Primeira.cpp
+++ src/fases/Fase_Primeira.cpp
@@ -21,7 +21,8 @@ void Fase_Primeira::inicializarMapa() {
 
   mapaEntidades[CHAR_INIM_FACIL] = []() -> Entidades::Entidade * {
     return new Entidades::Personagens::Inim_Facil(
-        CAMINHO_IMAGENS "/oposum.png", sf::IntRect(0, 2, 32, 25), {0, 0}, 70);
+        CAMINHO_IMAGENS "/frog-idle.png", sf::IntRect(0, 2, 32, 25), {0, 0},
+        70);
   };
 
   // TODO: mover para a fase 2
