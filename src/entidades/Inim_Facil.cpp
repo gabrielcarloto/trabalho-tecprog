aef4fafbcd7d2214ec04c9edca06baed1d122bb1
Gabriel Carloto <gabrielcidralcarloto@gmail.com>
2023-06-05 18:27:21 -0300

    adiciona fator pulo do sapo

--- src/entidades/Inim_Facil.cpp
+++ src/entidades/Inim_Facil.cpp
@@ -14,16 +14,17 @@ constexpr float DISTANCIA_X_PERSEGUIR_JOGADOR = TAMANHO_TILE * 3,
 
 namespace Jogo::Entidades::Personagens {
 Inim_Facil::Inim_Facil(const char *path, sf::Vector2f pos, float velo,
-                       bool pula)
-    : Inimigo(path, MALDADE_PADRAO_FACIL, pos, velo), pulaAleatoriamente(pula) {
+                       bool pula, float fator)
+    : Inimigo(path, MALDADE_PADRAO_FACIL, pos, velo), pulaAleatoriamente(pula),
+      fatorPulo(fator) {
   distanciaPerseguirJogadorX = DISTANCIA_X_PERSEGUIR_JOGADOR;
   distanciaPerseguirJogadorY = DISTANCIA_Y_PERSEGUIR_JOGADOR;
 }
 
 Inim_Facil::Inim_Facil(const char *path, sf::IntRect lim, sf::Vector2f pos,
-                       float velo, bool pula)
+                       float velo, bool pula, float fator)
     : Inimigo(path, lim, MALDADE_PADRAO_FACIL, pos, velo),
-      pulaAleatoriamente(pula) {
+      pulaAleatoriamente(pula), fatorPulo(fator) {
   distanciaPerseguirJogadorX = DISTANCIA_X_PERSEGUIR_JOGADOR;
   distanciaPerseguirJogadorY = DISTANCIA_Y_PERSEGUIR_JOGADOR;
 }
@@ -34,7 +35,7 @@ float Inim_Facil::perseguirJogador(sf::Vector2f posJogador) {
   float direcao = Inimigo::perseguirJogador(posJogador);
 
   if (pulaAleatoriamente && podePular && Uteis::chance(1))
-    pular(TAMANHO_TILE * 1.2);
+    pular(TAMANHO_TILE * fatorPulo);
 
   return direcao;
 }
@@ -43,7 +44,6 @@ void Inim_Facil::moverAleatoriamente() {
   Inimigo::moverAleatoriamente();
 
   if (pulaAleatoriamente && podePular && Uteis::chance(1))
-    pular(TAMANHO_TILE * 1.2);
+    pular(TAMANHO_TILE * fatorPulo);
 }
-
 } // namespace Jogo::Entidades::Personagens
--- src/entidades/Inim_Facil.h
+++ src/entidades/Inim_Facil.h
@@ -2,18 +2,21 @@
 #include "Inimigo.h"
 
 constexpr char CHAR_INIM_FACIL = 'F';
+constexpr float FATOR_PULO_PADRAO = 3;
 
 namespace Jogo::Entidades::Personagens {
 class Inim_Facil : public Inimigo {
 public:
-  Inim_Facil(const char *, sf::Vector2f = {0, 0}, float = 0, bool = true);
+  Inim_Facil(const char *, sf::Vector2f = {0, 0}, float = 0, bool = true,
+             float = FATOR_PULO_PADRAO);
   Inim_Facil(const char *, sf::IntRect, sf::Vector2f = {0, 0}, float = 0,
-             bool = true);
+             bool = true, float = FATOR_PULO_PADRAO);
 
   void executar() override;
 
 protected:
   bool pulaAleatoriamente;
+  float fatorPulo;
 
   void moverAleatoriamente() override;
   float perseguirJogador(sf::Vector2f) override;
